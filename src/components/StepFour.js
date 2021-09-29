c7428e121c45edd77ba589c80ea8f65671f855ea
Abid Raza <abidr.w@gmail.com>
2021-09-30 02:25:24 +0500

    fix formsubmission id - random func

--- src/components/StepFour.js
+++ src/components/StepFour.js
@@ -35,14 +35,7 @@ const StepFour = (props) => {
   };
 
   function makeid(length) {
-    var result = "";
-    var characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+    return Math.floor(Math.random() * 999999999);
   }
 
   const postUserList = () => {
