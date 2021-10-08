b92ff1fdf335eb18c7b65b12394294a63386a18c
Abid Raza <abidr.w@gmail.com>
2021-10-08 22:15:35 +0500

    distance api fix

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 export default class Constants {
     // static API_URL = '/wp-admin/admin-ajax.php';
-    static API_URL = 'http://localhost/wordpress/wp-admin/admin-ajax.php';
-    // static API_URL = 'https://qa.phlebotomynetwork.com/wp-admin/admin-ajax.php';
+    // static API_URL = 'http://localhost/wordpress/wp-admin/admin-ajax.php';
+    static API_URL = 'https://qa.phlebotomynetwork.com/wp-admin/admin-ajax.php';
 }
