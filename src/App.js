e866be31551ba0668a14861cef626d3879e7adc5
Abid Raza <abidr.w@gmail.com>
2021-10-26 20:25:01 +0500

    google api hidden now

--- src/App.js
+++ src/App.js
@@ -12,10 +12,40 @@ function App() {
   var apiLoading = false;
   useEffect(() => {
     localStorage.removeItem("bfusers");
+
     if (bfusers.length == 0) {
       getUserList();
     }
+
+    getConfig().then(res => {
+      const currentScript = document.getElementById('google-map-js');
+
+      if(!currentScript && res?.google_maps_api_key) {
+        const script = document.createElement("script");
+
+        script.src = "https://maps.googleapis.com/maps/api/js?key=" + res.google_maps_api_key + "&libraries=places&language=en";
+        script.async = true;
+        script.id = 'google-map-js';
+
+        document.head.appendChild(script);
+      }
+    });
+
   }, []);
+
+  const getConfig = () => new Promise( async (resolve, reject) => {
+    const url = Constants.API_URL + "?action=get_nppn_settings";
+
+    const options = {
+      method: "GET",
+      url,
+    };
+
+    let response = await axios(options);
+    localStorage.setItem("nppn-settings", JSON.stringify(response));
+
+    resolve(response.data);
+  });
 
   const getUserList = () => {
     if (apiLoading) {
